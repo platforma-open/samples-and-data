236c2208591f40a25129cea9013370cc8453f992
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-10-04 16:20:46 +0300

    wip

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,13 +3,5 @@ import '@platforma-sdk/ui-vue/dist/style.css';
 import { createApp } from 'vue';
 import { sdkPlugin } from './app';
 import { BlockLayout } from '@platforma-sdk/ui-vue';
-import { LicenseManager } from '@ag-grid-enterprise/core';
-
-const agGridLicense = getEnvironmentValue('AGGRID_LICENSE');
-console.log(agGridLicense);
-if (agGridLicense) {
-  LicenseManager.setLicenseKey(agGridLicense);
-  console.log(agGridLicense);
-}
 
 createApp(BlockLayout).use(sdkPlugin).mount('#app');
